38f83d923aabe5bd1b14be9e83a42cfa184a5742
Jai <jaipandey2508@gmail.com>
2026-05-05 01:24:28 +0530

    modified app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
-const express = require("express")
+const express = require("express");
 const cookieParser = require("cookie-parser");
 
-const authRouter = require("./routes/auth.routes.js");
-
-
-
-
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes.js");
+const accountRouter = require("./routes/account.routes.js");
+const transactionRoutes = require("./routes/transaction.routes.js");
+
+/**
+ * - Use Routes
+ */
+
+app.get("/", (req, res) => {
+  res.send("Ledger Service is up and running");
+});
+
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+app.use("/api/transactions", transactionRoutes);
 
-
-
-module.exports = app
+module.exports = app;
